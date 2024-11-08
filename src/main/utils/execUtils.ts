5496dd80c2255a42be18eb91603d429f14c38001
dftre <3066417822@qq.com>
2024-11-08 13:50:12 +0800

    update

--- src/main/utils/execUtils.ts
+++ src/main/utils/execUtils.ts
@@ -1,31 +1,40 @@
-import { ChildProcess, exec, ExecException, ExecOptions } from 'child_process';
-
-/**
- * 在系统 shell 中执行命令或一系列命令。
- * 
- * @param cmd - 要执行的命令，可以是单个命令字符串或命令数组。
- * @param options - 执行命令的选项，默认为空对象。{}
- * @returns 返回一个 Promise，解析时返回包含子进程和标准输出的对象，拒绝时返回包含子进程和标准错误的对象。
- */
-export function runExec(cmd: string | string[], options: ExecOptions = {}) {
- // options：{
-//     uid?: number | undefined;
-//     gid?: number | undefined;
-//     cwd?: string | URL | undefined;
-//     env?: NodeJS.ProcessEnv | undefined;
-//     windowsHide?: boolean | undefined; @default false
-//     timeout?: number | undefined; @default 0
-//     shell?: string | undefined;
-//     signal?: AbortSignal | undefined;
-//     maxBuffer?: number | undefined;
-//     killSignal?: NodeJS.Signals | number | undefined;
-// }
-    return new Promise((resolve: (value: { p: ChildProcess, stdout: (string | Buffer) }) => void, reject: (value: { p: ChildProcess, stderr: (string | Buffer) }) => void) => {
-        let cmdStr = Array.isArray(cmd) ? cmd.join(' ') : cmd
-        const p = exec(cmdStr, options, (error: ExecException | null, stdout: string | Buffer, stderr: string | Buffer) => {
-            if (stdout) resolve({ p, stdout })
-            if (stderr) reject({ p, stderr })
-            if (error) throw error
-        })
-    })
+import { exec, ExecException, ExecOptions, spawn, SpawnOptions } from 'child_process';
+import { ObjectEncodingOptions } from 'node:fs';
+type Args = string[] | { [key: string]: (string | number | null | undefined | boolean) }
+type Parse = {
+    joinKeyValue?: string,
+    beforKey?: string
 }
+function parseArgs(args: Args, options?: Parse) {
+    if (Array.isArray(args)) {
+        return args.join(' ')
+    } else {
+        let argList: string[] = []
+        let beforKey: string = options ? options.beforKey || '' : ''
+        let joinKeyValue: string = options ? options.joinKeyValue || ' ' : ' '
+        for (let key in args) {
+            if (args[key] === null || args[key] === undefined) {
+                argList.push(`${beforKey}${key}`)
+            } else if (typeof args[key] !== 'object' && typeof args[key] !== 'function') {
+                argList.push(`${beforKey}${key}${joinKeyValue}${args[key]}`)
+            }
+        }
+        return argList.join(' ')
+    }
+}
+export function runExec(cmd: string | string[], args?: Args, options?: (ObjectEncodingOptions & ExecOptions & Parse) | null, callback?: ((error: ExecException | null, stdout: string | Buffer, stderr: string | Buffer) => void)) {
+    let cmdStr = Array.isArray(cmd) ? cmd.join(' ') : cmd
+    if (args) cmdStr + " " + parseArgs(args)
+    return exec(cmdStr, options, callback)
+}
+
+export function runSpawn(cmd: string | string[], args?: Args, options: SpawnOptions & Parse = {}) {
+    let cmdStr = Array.isArray(cmd) ? cmd.join(' ') : cmd
+    if (args) cmdStr + " " + parseArgs(args)
+    return spawn(cmdStr, options)
+}
+
+// runtime\\python.exe -m api_v2 -a 127.0.0.1 -p
+// fun("runtime\\python.exe",{"-m":"api_v2","-a":"127.0.0.1","-p":null})
+// fun("runtime\\python.exe",["-m", "api_v2", "-a", "127.0.0.1", "-p"])
+// fun(["runtime\\python.exe", "-m", "api_v2", "-a", "127.0.0.1", "-p"])
