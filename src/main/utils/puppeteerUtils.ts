872081a4c879912bfa8cbaaa6d136d252957dc91
dftre <3066417822@qq.com>
2025-07-15 09:07:52 +0800

    优化部分内容

--- src/main/utils/puppeteerUtils.ts
+++ src/main/utils/puppeteerUtils.ts
@@ -1,9 +1,10 @@
 
-import { app } from 'electron'
+import { app, BrowserWindow } from 'electron'
 import type { Browser } from 'puppeteer-core'
 import * as  Puppeteer from 'puppeteer-core'
 import puppeteer from 'puppeteer-extra'
 import BaseWindow from '../window/BaseWindow'
+import * as UUID from 'uuid'
 import pie from 'puppeteer-in-electron'
 let browser: Browser | null = null
 
@@ -14,6 +15,7 @@ export function getBrowser() {
 
 export async function getPage(window: BaseWindow) {
     if (window.browserWindow) {
+        await waitForPageLoad(window.browserWindow)
         return await pie.getPage(getBrowser(), window.browserWindow)
     } else {
         return null
@@ -24,4 +26,35 @@ export async function initialize() {
     const port = app.commandLine.getSwitchValue("remote-debugging-port");
     if (!port) await pie.initialize(app)
     browser = await pie.connect(app, (puppeteer as unknown as typeof Puppeteer))
+}
+
+
+async function waitForPageLoad(window: BrowserWindow): Promise<void> {
+    const tempGuid = UUID.v4()
+    try {
+        if (window.webContents.getURL() === '') {
+            await window.webContents.loadURL('about:blank')
+        }
+        await window.webContents.executeJavaScript(`window.tempPuppeteerId = "${tempGuid}"`)
+        await getBrowser().waitForTarget(
+            async (target) => {
+                if (target.type() !== 'page') return false
+                try {
+                    const page = await target.page()
+                    if (!page) return false
+                    return (await page.evaluate('window.tempPuppeteerId')) === tempGuid
+                } catch (e) {
+                    return false
+                }
+            },
+            { timeout: 5000 }
+        )
+    } catch (error: any) {
+        window.destroy()
+        throw new Error('等待 Puppeteer 目标超时，无法找到新窗口。')
+    } finally {
+        if (!window.isDestroyed()) {
+            await window.webContents.executeJavaScript('delete window.tempPuppeteerId')
+        }
+    }
 }
